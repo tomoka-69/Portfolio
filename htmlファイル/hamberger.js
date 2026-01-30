4143c9334984f2683fa32d97ffd4df42645feef2
sasaki <t.sasaki.sys24@morijyobi.ac.jp>
2026-01-30 10:57:41 +0900

    ポートフォリオ

---
+++ htmlファイル/hamberger.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(function() {
+  $('.hamburger').click(function() {
+    // メニューの開閉状態を切り替える
+    $('.menu').toggleClass('open');
+
+    // ハンバーガーボタンのアクティブクラスを切り替える
+    $(this).toggleClass('active');
+  });
+});
